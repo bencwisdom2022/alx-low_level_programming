ef5160203d7009ea66dc450c1681e206ae57474c
bencwisdom2022 <bencwisdom@gmail.com>
2022-09-12 19:15:50 +0100

    my c program commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-*main - main block of the program
-*Return: 0
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0)
-} 
+return (0);
+}
